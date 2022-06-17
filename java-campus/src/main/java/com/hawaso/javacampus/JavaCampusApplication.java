2e5bd70f35fa3b172137cd4d550633b219fc9619
SpringCampus <yhkwon@wizardlab.co.kr>
2022-06-17 11:22:54 +0900

    JavaCampus Project Creation

---
+++ java-campus/src/main/java/com/hawaso/javacampus/JavaCampusApplication.java
@@ -0,0 +1,13 @@
+package com.hawaso.javacampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCampusApplication.class, args);
+	}
+
+}
---
+++ java-campus/src/test/java/com/hawaso/javacampus/JavaCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hawaso.javacampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
